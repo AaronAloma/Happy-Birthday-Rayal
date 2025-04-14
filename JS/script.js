064e3fb47d38d1770c06855cf8d683b019435683
AaronAloma <aaronsgithub@gmail.com>
2025-04-14 23:24:31 +0000

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,4 @@
+//Responsive Menu
 const menuBtn = document.querySelector('.menu-btn');
 
 const navigation = document.querySelector('.navigations').innerHTML;
@@ -24,13 +25,13 @@ new Swiper('.card-wrapper', {
     dynamicBullets: true,
   },
 
-  // Navigation arrows
+  // Navigation Arrows
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
 
-  //Responsive breakpoints
+  //Responsive Breakpoints
   breakpoints: {
     0: {
         slidesPerView: 1
@@ -41,5 +42,32 @@ new Swiper('.card-wrapper', {
     1024: {
         slidesPerView: 3
     },
+  }
+});
+
+// Popup Image
+document.querySelectorAll(".clickable-image").forEach(image => {
+  image.addEventListener("click", function () {
+    const popup = document.getElementById("popup");
+    const popupImg = document.getElementById("popup-img");
+
+    // Set the popup image source to the clicked image
+    popupImg.src = this.src;
+
+    // Show the popup
+    popup.classList.remove("hidden");
+  });
+});
+
+// Close the popup when the close button is clicked
+document.getElementById("close").addEventListener("click", function () {
+  const popup = document.getElementById("popup");
+  popup.classList.add("hidden");
+});
+
+// Close the popup when clicking outside the image
+document.getElementById("popup").addEventListener("click", function (event) {
+  if (event.target.id === "popup") {
+    this.classList.add("hidden");
   }
 });
